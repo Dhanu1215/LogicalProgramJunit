f2ba497eed4d817c7e0e0c6800439f604d3321a3
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-25 11:16:16 +0530

    Added: Monthly Payment file is added

---
+++ src/com/blp/logicalprogramJunit/MonthlyPayment.java
@@ -0,0 +1,28 @@
+package com.blp.logicalprogramJunit;
+
+import java.util.Scanner;
+
+/**
+ * Calculate Monthly Payment
+ * Use following formula
+ * payment = (p*r) / 1-(1 + r)^(-n)
+ * Where, n = 12 * Y, r = R / (12 * 100)
+ */
+public class MonthlyPayment {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //Use Scanner to read values
+        System.out.println("Enter Principle amount, Rate of interest and Year");
+        int principleAmount = sc.nextInt();
+        int rateOfInterest = sc.nextInt();
+        int year = sc.nextInt();
+        System.out.println("Monthly payment = " +getPayment(principleAmount, rateOfInterest, year));
+    }
+
+    private static double getPayment(int principleAmount, int rateOfInterest, int year) {
+        double rate = (double)(Math.floorDiv(rateOfInterest, (12 * 100)));
+        double noOfYear = 12 * year;
+        double payment = (double)((principleAmount * rate) / 1 - Math.pow((1 + rate), (-noOfYear)));
+        return (payment);
+    }
+}
